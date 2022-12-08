9d610bad1a841e523e817d452fe827c0cb1d2321
Marcin Jasiński <alpinus1@gmail.com>
2022-12-08 19:31:02 +0100

    meal list -> form -> button, input, amount, labels

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -17,7 +17,7 @@ const DUMMY_MEALS = [
 	},
 	{
 		id: 'm3',
-		name: 'Barbecue Burger',
+		name: 'BBQ Burger',
 		description: 'American, raw, meaty',
 		price: 12.99,
 	},
--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,17 +1,20 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = props => {
 	const price = `$${props.price.toFixed(2)}`;
 
 	return (
 		<li className={classes.meal}>
-			<div>
+			<div className={classes['meal-list']}>
 				<h3 className={classes.name}>{props.name}</h3>
 				<div className={classes.description}>{props.description}</div>
 				<div className={classes.price}>{price}</div>
 			</div>
 
-			<div></div>
+			<div>
+				<MealItemForm />
+			</div>
 		</li>
 	);
 };
