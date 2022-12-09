dcb7f93f0b143c9040c610618b2817d72623b1e7
Marcin Jasiński <alpinus1@gmail.com>
2022-12-09 15:40:48 +0100

    changed dummy date to firebase, fetching meals

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,54 +1,35 @@
+import { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'French Fries',
-		description: 'Best fries from fresh potatoes',
-		price: 11.99,
-	},
-	{
-		id: 'm2',
-		name: 'Mac and Cheese',
-		description: 'Classic, worldwide burger.',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'BBQ Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Salad Bowls',
-		description: 'Healthy mixed salads. So fresh !',
-		price: 7.99,
-	},
-	{
-		id: 'm5',
-		name: 'Black Reaper',
-		description: 'Can you handle our SO-SPICY-BURGER?',
-		price: 9.99,
-	},
-	{
-		id: 'm6',
-		name: 'Polish Pierogis',
-		description: 'Classic Polish dumplings with cheese',
-		price: 10.99,
-	},
-	{
-		id: 'm7',
-		name: 'Lasagna',
-		description: 'Over 5000 Calories in once. Dig it!',
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meal => (
+	const [meals, setMeals] = useState([]);
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				'https://click-n-eat-15db4-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+			);
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+
+			setMeals(loadedMeals);
+		};
+		fetchMeals();
+	}, []);
+
+	const mealsList = meals.map(meal => (
 		<MealItem
 			id={meal.id}
 			key={meal.id}
