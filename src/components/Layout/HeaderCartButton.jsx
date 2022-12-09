6f0f5c58eae42a0ad8a1d1dabcb67d646844ac09
Marcin Jasiński <alpinus1@gmail.com>
2022-12-09 12:37:10 +0100

    Context added, logic for number items in cart

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = props => {
+	const cartCtx = useContext(CartContext);
+
+	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+		return curNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} onClick={props.onClick}>
 			<span className={classes.icon}>
@@ -10,7 +18,7 @@ const HeaderCartButton = props => {
 
 			<span>Cart</span>
 
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
